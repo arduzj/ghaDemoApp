576aa8fe6b61ab00da9a296be6c07ecd40feff41
arduzj <arduzj@gmail.com>
2024-05-30 14:40:44 -0400

    gitHub Actions deployed

--- components/jumbotron.js
+++ components/jumbotron.js
@@ -18,6 +18,7 @@ const Jumbotron = () =>
                 border-radius: 3px;
             }
         `}</style>
+        
         <div className="jumbotron jumbotron-fluid bg-dark mb-0" alt="Photo by Benjamin Child on Unsplash - https://unsplash.com/photos/GWe0dlVD9e0">
             <div className="container text-white">
                 <h1 className="display-2 text-white d-none d-lg-block">
